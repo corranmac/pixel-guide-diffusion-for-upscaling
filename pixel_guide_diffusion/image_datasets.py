e4190cbaba816c30e5b99f198e9f55a580c6fcc6
corranmac <corranmac@hotmail.co.uk>
2022-06-14 01:01:40 +0100

    Update image_datasets.py

--- pixel_guide_diffusion/image_datasets.py
+++ pixel_guide_diffusion/image_datasets.py
@@ -42,7 +42,7 @@ def load_data(
     if class_cond:
         # Assume classes are the first part of the filename,
         # before an underscore.
-        class_names = [bf.basename(path).split("_")[0] for path in all_files]
+        class_names = [os.basename(path).split("_")[0] for path in all_files]
         sorted_classes = {x: i for i, x in enumerate(sorted(set(class_names)))}
         classes = [sorted_classes[x] for x in class_names]
     dataset = ImageDataset(
